6c182c114937eb64fd3f15a3d58abd31132701f1
Ulrich Lehner <me@ulrichlehner.com>
2017-09-12 10:07:48 +0200

    Improved css build

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,54 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 const PATH_DIST = "dist/";
 const PATH_SRC = "src/";
 
-gulp.task('serve', ['sass'], function () {
+const TARGET_CSS = "inspirespace";
+
+gulp.task('serve', ['sass', 'sass-min'], function () {
   browserSync.init({
     server: "./"
   });
 
-  gulp.watch(PATH_SRC + 'scss/*.scss', ['sass']);
+  gulp.watch(PATH_SRC + 'scss/*.scss', ['sass', 'sass-min']);
   gulp.watch('*.html').on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
   return gulp.src(PATH_SRC + 'scss/main.scss')
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(rename(TARGET_CSS + ".css"))
     .pipe(gulp.dest(PATH_DIST + 'css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('font', function() {
+gulp.task('sass-min', function () {
+  return gulp.src(PATH_SRC + 'scss/main.scss')
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(rename(TARGET_CSS + ".min.css"))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(PATH_DIST + 'css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('font', function () {
   return gulp.src([
     'node_modules/metropolis-font/Webfonts/**/*.woff',
     'node_modules/metropolis-font/Webfonts/**/*.woff2'])
-    .pipe(rename({dirname: ''}))
+    .pipe(rename({ dirname: '' }))
     .pipe(gulp.dest(PATH_DIST + "font"));
 });
 
